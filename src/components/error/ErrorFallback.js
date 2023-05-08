297369a1eba14f3f92a933400dae85622a4dc845
chandrabhanu <dcv.kalansooriya@gmail.com>
2023-05-08 11:55:40 +0530

    bug fixes: UI, data format

--- src/components/error/ErrorFallback.js
+++ src/components/error/ErrorFallback.js
@@ -4,7 +4,7 @@ const ErrorFallback = ({ error, resetErrorBoundary }) => {
   return (
     <div className="error-wrap">
       <div className="error">
-        <div className="error-header">Error header</div>
+        <div className="error-header">Network Error</div>
         <div className="error-body">
           <img src="/images/error-100.png" alt="error"></img>
           <p className="error-message">{error.message}</p>
